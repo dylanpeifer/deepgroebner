72e29496ccb379032cf4f7c73fdb2f5992c55d52
Dylan Peifer <djp282@cornell.edu>
2020-08-17 11:26:16 -0500

    Update format in DQN

--- deepgroebner/dqn.py
+++ deepgroebner/dqn.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 import tensorflow as tf
-import time
 
 
 class CyclicMemory:
@@ -14,7 +13,7 @@ class CyclicMemory:
         self.position = 0
         self.size = 0
 
-    def push(self, transition):
+    def store(self, transition):
         """Save a transition."""
         self.buffer[self.position] = transition
         self.position = (self.position + 1) % self.capacity
@@ -33,14 +32,12 @@ class DQNAgent:
     """A deep Q network agent."""
 
     def __init__(self, network, learning_rate=0.00025,
-                 memory_capacity=100000, batch_size=32,
-                 gamma=0.99,
+                 memory_capacity=100000, batch_size=32, gamma=0.99,
                  epsilon_min=0.01, decay_rate=0.999, decay_mode='exponential',
                  start_steps=1000, replay_freq=1, target_update_freq=100, epsilon_decay_freq=1,
-                 double=False):
-        self.action_size = network.output_shape[1]
-        self.onlineModel = self._buildModel(network, learning_rate)
-        self.targetModel = self._buildModel(network, learning_rate)
+                 double=False, action_dim_fn=lambda s: s[0]):
+        self.online_model = self._build_model(network, learning_rate)
+        self.target_model = self._build_model(network, learning_rate)
         
         self.memory = CyclicMemory(memory_capacity)
         self.batch_size = batch_size
@@ -60,36 +57,38 @@ class DQNAgent:
         
         self.double = double
 
+        self.action_dim_fn = action_dim_fn
+
     def act(self, state, epsilon=None):
         """Choose an action for the given state using an epsilon-greedy policy."""
         if epsilon is None:
             epsilon = self.epsilon
         if np.random.rand() < epsilon:
-            return np.random.randint(self.action_size)
+            return np.random.randint(self.action_dim_fn(state.shape))
         else:
             return np.argmax(self.predict(state))
 
     def predict(self, state, model='online'):
         """Return the predicted Q-values for the given state."""
         if model == 'online':
-            return self.onlineModel.predict(np.expand_dims(state, axis=0))[0]
+            return self.online_model(state[np.newaxis])[0]
         elif model == 'target':
-            return self.targetModel.predict(np.expand_dims(state, axis=0))[0]
+            return self.target_model(state[np.newaxis])[0]
         else:
             raise ValueError
 
-    def predictBatch(self, states, model='online'):
+    def predict_batch(self, states, model='online'):
         """Return the predicted Q-values for a batch of states."""
         if model == 'online':
-            return self.onlineModel.predict(states)
+            return self.online_model(states)
         elif model == 'target':
-            return self.targetModel.predict(states)
+            return self.target_model(states)
         else:
             raise ValueError
 
     def remember(self, state, action, reward, next_state, done):
         """Add a state transition to memory."""
-        self.memory.push((state, action, reward, next_state, done))
+        self.memory.store((state, action, reward, next_state, done))
 
     def replay(self, batch_size):
         """Train on batch_size transitions from memory."""
@@ -104,18 +103,18 @@ class DQNAgent:
         next_states = np.array(next_states)
         dones = np.array(dones)
         
-        pred_online = self.predictBatch(states, model='online')
-        next_pred_target = self.predictBatch(next_states, model='target')     
+        pred_online = self.predict_batch(states, model='online').numpy()
+        next_pred_target = self.predict_batch(next_states, model='target').numpy()
         if self.double:
-            next_pred_online = self.predictBatch(next_states, model='online')
+            next_pred_online = self.predict_batch(next_states, model='online')
             update = rewards + (dones == False) * self.gamma * next_pred_target[np.arange(next_pred_target.shape[0]), np.argmax(next_pred_online, axis=1)]
         else:
             update = rewards + (dones == False) * self.gamma * np.max(next_pred_target, axis=1)   
         pred_online[np.arange(pred_online.shape[0]), actions] = update
 
-        self.onlineModel.fit(states, pred_online, verbose=0)
+        self.online_model.fit(states, pred_online, verbose=0)
 
-    def decayEpsilon(self):
+    def decay_epsilon(self):
         """Decrease the value of epsilon."""
         if self.decay_mode == 'linear':
             self.epsilon = max(self.epsilon_min, self.epsilon - self.decay_rate)
@@ -124,11 +123,11 @@ class DQNAgent:
         else:
             raise ValueError
 
-    def updateTargetModel(self):
+    def update_target_model(self):
         """Copy online model weights to target model."""
-        self.targetModel.set_weights(self.onlineModel.get_weights())
+        self.target_model.set_weights(self.online_model.get_weights())
         
-    def test(self, env, episodes, epsilon=None, render=False):
+    def test(self, env, episodes, epsilon=None):
         """Test the agent for given episodes on given environment."""
         rewards = np.zeros(episodes)
         for i in range(episodes):
@@ -138,9 +137,6 @@ class DQNAgent:
                 action = self.act(state, epsilon=epsilon)
                 state, reward, done, _ = env.step(action)
                 rewards[i] += reward
-                if render:
-                    env.render()
-                    time.sleep(0.05)
         return rewards
     
     def explore(self, env, steps, epsilon=None):
@@ -182,9 +178,9 @@ class DQNAgent:
                     if self.steps % self.replay_freq == 0:
                         self.replay(self.batch_size)
                     if self.steps % self.target_update_freq == 0:
-                        self.updateTargetModel()
+                        self.update_target_model()
                     if self.steps % self.epsilon_decay_freq == 0:
-                        self.decayEpsilon()
+                        self.decay_epsilon()
 
                     epoch_steps += 1
                     if epoch_steps >= steps:
@@ -193,19 +189,19 @@ class DQNAgent:
             if test_episodes is not None:
                 rewards[epoch] = np.mean(self.test(test_env, test_episodes, epsilon=test_epsilon))       
             if savefile is not None:
-                self.saveOnlineModel(str(epoch) + savefile)     
+                self.save_online_model(str(epoch) + savefile)     
             if verbose == 1:
                 print("\rEpoch {}/{} - avg_reward: {}".format(epoch+1, epochs, rewards[epoch]), end="")
                 
         return rewards
 
-    def saveOnlineModel(self, filename):
-        self.onlineModel.save_weights(filename)
+    def save_online_model(self, filename):
+        self.online_model.save_weights(filename)
 
-    def loadOnlineModel(self, filename):
-        self.onlineModel.load_weights(filename)
+    def load_online_model(self, filename):
+        self.online_model.load_weights(filename)
 
-    def _buildModel(self, network, learning_rate):
+    def _build_model(self, network, learning_rate):
         model = tf.keras.models.clone_model(network)
         loss = 'mse'
         optimizer = tf.keras.optimizers.Adam(learning_rate)
