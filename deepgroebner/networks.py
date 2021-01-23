c075f56cb92b339030d8f278791d4716e09615f7
Christian Varner <christianvarner@Christians-MacBook-Pro.local>
2021-01-22 23:14:37 -0500

    Adding layers

--- deepgroebner/networks.py
+++ deepgroebner/networks.py
@@ -565,7 +565,7 @@ class TransformerPMLP(tf.keras.Model):
 
     """
 
-    def __init__(self, dim, hidden_dim, num_layers = 1, activation='relu', final_activation='log_softmax'):
+    def __init__(self, dim, hidden_dim, num_layers = 1, n_heads = 4, activation='relu', final_activation='log_softmax'):
         super(TransformerPMLP, self).__init__()
         self.embedding = ParallelEmbeddingLayer(dim, [], final_activation=activation)
         self.attn = []
